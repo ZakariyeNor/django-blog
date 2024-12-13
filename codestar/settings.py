8c737b6be8c4b588b4394c9df6aa0d854d3bcc1b
Zakariye Farah Nor <0e6xrww7@students.codeinstitute.net>
2024-12-13 11:30:45 +0000

    add view or html templates and diplay content

--- codestar/settings.py
+++ codestar/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['8000-zakariyenor-djangoblog-2wr6zg2jiep.ws.codeinstitute-ide.net','.herokuapp.com']
 
