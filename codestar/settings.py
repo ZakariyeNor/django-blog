4bdd43e71312f79d444e273f98c42ca711899d5f
Zakariye Farah Nor <0e6xrww7@students.codeinstitute.net>
2024-12-17 11:24:21 +0000

    change debug to false for deployment reason

--- codestar/settings.py
+++ codestar/settings.py
@@ -28,7 +28,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['8000-zakariyenor-djangoblog-2wr6zg2jiep.ws.codeinstitute-ide.net','.herokuapp.com']
 
